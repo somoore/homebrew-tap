c30aedda7484fe47e2f23964ea8c75d77b01d615
Scott Moore <scott@moore.cloud>
2026-01-05 11:44:02 -0600

    Update to v0.2.0 - Full CLI rewrite with subcommands

--- Formula/rust-ssm.rb
+++ Formula/rust-ssm.rb
@@ -2,9 +2,9 @@
 # frozen_string_literal: true
 
 class RustSsm < Formula
-  desc "AWS SSM parameter retrieval tool"
+  desc "Powerful CLI tool for AWS Systems Manager Parameter Store"
   homepage "https://github.com/somoore/rust-ssm"
-  url "https://github.com/somoore/rust-ssm.git", tag: "v0.1.1"
+  url "https://github.com/somoore/rust-ssm.git", tag: "v0.2.0"
   license "MIT"
 
   depends_on "rust" => :build
@@ -15,6 +15,7 @@ class RustSsm < Formula
 
   test do
     assert_path_exists bin/"rust-ssm"
-    assert_match "rust-ssm", shell_output("#{bin}/rust-ssm --version")
+    assert_match "rust-ssm 0.2.0", shell_output("#{bin}/rust-ssm --version")
+    assert_match "list", shell_output("#{bin}/rust-ssm --help")
   end
 end
